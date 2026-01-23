c35ba71a041e5ba5a27d243f1492da3aeb87fd7f
theguyinpink <clement.carre.prevert.pro@gmail.com>
2026-01-23 22:26:30 +0100

    Admin and Product Page

--- middleware.ts
+++ middleware.ts
@@ -28,5 +28,9 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/admin/:path*",
+    "/api/admin/:path*",
+  ],
 };
+
